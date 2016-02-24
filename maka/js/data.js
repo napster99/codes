cd06b29fab36a297a1c03dd917b42a0c5e3fca9c
zhangxiaojian <809270105@qq.com>
2016-02-24 19:00:02 +0800

    maka

---
+++ maka/js/data.js
@@ -0,0 +1,126 @@
+var datas = {
+  "errcode": 0,
+  "errmsg": "",
+  "response": {
+    "code": 0,
+    "message": "SUCCESS",
+    "data": {
+      "id": "146166",
+      "uid": "115165",
+      "area_code": "330304",
+      "status": "1",
+      "location": "温州市瓯海区",
+      "pond_id": "63759",
+      "topic_id": "17",
+      "fishing_time": "2016-02-23 00:00",
+      "pond_rank": "-1",
+      "is_recommended": "0",
+      "pics": "[\"http:\/\/img.qiniu.yddapp.com\/FmuJgqYhPuoRTb5EkLGUZ8FjTHGI?jpeg-151774-600-400\"]",
+      "desc": "钓鱼的乐趣",
+      "fish_extra": "[{\"img\":\"http://img.qiniu.yddapp.com/Fitg_TsyrV8K97gYR-YyoTRh2Im0?jpeg-389705-1280-960\"},{\"text\":\"今天刚刚下班，吃了晚饭带上装备。来到三洋这里开始打窝。等了一个小时左右开始有口，然后就一直不停的吃口。不停的上鱼感觉跟做似的天亮了一看居然\"},{\"img\":\"http://img.qiniu.yddapp.com/FhysMYfmCpgDLahQNaxSiQmQs3yB?jpeg-320118-960-1280\"},{\"text\":\"啊啊啊啊\"},{\"img\":\"http://img.qiniu.yddapp.com/Fitg_TsyrV8K97gYR-YyoTRh2Im0?jpeg-389705-1280-960\"},{\"img\":\"http://img.qiniu.yddapp.com/FndKfNAKk3rA18y4_akhXg3qi3Ro?jpeg-298847-960-1280\"},{\"text\":\"不不不不不\"},{\"img\":\"http://img.qiniu.yddapp.com/Fj-cRkqS0Lea4tccKKexL2mvscx6?jpeg-253183-960-1280\"}]",
+      "fish_extra2": "{\"bait_brand\":\"\u6fa1\u5143\u7d20\u5927\u677f\u9cab\",\"fishing_rod_brand\":\"5.4\",\"weight\":\"50\",\"species\":\"\u7530\u9c7c\",\"main_line\":\"1.5\",\"child_line\":\"0.8\"}",
+      "comment_total": "0",
+      "zan_total": "0",
+      "dateline": "1456190410",
+      "ctime": "1456190410",
+      "wether_info": "",
+      "lng": "120.5992506585458",
+      "lat": "27.9526899783634",
+      "p_name": "温周一家人农家乐",
+      "is_dj_detail": true,
+      "dj_data": {
+        "elite": false,
+        "elite_value": "0",
+        "serial_number": "1"
+      },
+      "following": 0,
+      "uname": "新手dy",
+      "gender": "1",
+      "avatar": "http://avatar.qiniu.yddapp.com/FuOeOx-qvy8Pnx9ULUOLuBs_hEYL?jpeg-22377-120-120",
+      "pretty_time": "2小时前",
+      "img_bait": "",
+      "img_species": "",
+      "zans": [
+        {
+          "uid": "88853",
+          "avatar": "http://tb.himg.baidu.com/sys/portrait/item/2c93d0ecc3cd676faa28?t=1433486119"
+        },
+        {
+          "uid": "11086",
+          "avatar": "http://avatar.qiniu.yddapp.com/FtbdETXKkMsxZ8ahxcHCzhRahyPw?jpeg-6692-110-110"
+        },
+        {
+          "uid": "88769",
+          "avatar": "http://tb.himg.baidu.com/sys/portrait/item/c513d0a1caafcdb75f3132333435363425?t=1429352274"
+        },
+        {
+          "uid": "12363",
+          "avatar": "http://avatar.qiniu.yddapp.com/Fq7zUO-rdfLphppN2CtOvxHFjcdm?jpeg-3786-110-110"
+        },
+        {
+          "uid": "88263",
+          "avatar": "http://tb.himg.baidu.com/sys/portrait/item/d9c8ced2b7a2cfd6cdf8c3fbb2bbb3a4e217?t=1441060564"
+        },
+        {
+          "uid": "88722",
+          "avatar": "http://tb.himg.baidu.com/sys/portrait/item/19d3bdadcbd5baecb3e66e71?t=1441363259"
+        },
+        {
+          "uid": "12469",
+          "avatar": "http://avatar.qiniu.yddapp.com/FolU5JHYLN66_ztLooe2dvliQNjz?jpeg-6105-110-110"
+        },
+        {
+          "uid": "88627",
+          "avatar": "http://tb.himg.baidu.com/sys/portrait/item/7ba0bacfbea7cab1b4fa9000"
+        },
+        {
+          "uid": "749",
+          "avatar": "http://avatar.qiniu.yddapp.com/FqJaFwoaW8sygUzGTtcJlRkjcZOc?jpeg-4436-110-110"
+        },
+        {
+          "uid": "89004",
+          "avatar": "http://tb.himg.baidu.com/sys/portrait/item/e626d6f0b9e2d7b7d0d1644a?t=1403265906"
+        },
+        {
+          "uid": "88743",
+          "avatar": "http://tb.himg.baidu.com/sys/portrait/item/9d8c6c69c3c0c3c0cab1b9e24548?t=1423477991"
+        },
+        {
+          "uid": "88091",
+          "avatar": "http://tb.himg.baidu.com/sys/portrait/item/eb8a94d88197333b?t=1446174204"
+        },
+        {
+          "uid": "10714",
+          "avatar": "http://avatar.qiniu.yddapp.com/FttDI-SIH4a4LlxenI8jgjPFSQc1?jpeg-5578-110-110"
+        },
+        {
+          "uid": "12580",
+          "avatar": "http://avatar.qiniu.yddapp.com/Fn8bl6kIoZOW4-CkkJN-kTd-7bEf?jpeg-5028-110-110"
+        },
+        {
+          "uid": "11213",
+          "avatar": "http://avatar.qiniu.yddapp.com/Fmpmuo_WyKq3tYUszXeQuU7POZvK?jpeg-5179-110-110"
+        },
+        {
+          "uid": "521",
+          "avatar": "http://avatar.qiniu.yddapp.com/FiGlITjw4WaKhFfpE24hLeYjcau1?jpeg-5475-110-110"
+        },
+        {
+          "uid": "12174",
+          "avatar": "http://avatar.qiniu.yddapp.com/Fj8XcNbXWwrX0ctVLx8umHIT3hBk?jpeg-5887-110-110"
+        },
+        {
+          "uid": "10741",
+          "avatar": "http://avatar.qiniu.yddapp.com/FkLQkZFpsp6cmEtJobU9i7Zqptle?jpeg-5640-110-110"
+        },
+        {
+          "uid": "13359",
+          "avatar": "http://avatar.qiniu.yddapp.com/Fu66Fx41W5cc48S8YIjGYmtRS2kO?jpeg-4300-110-110"
+        }
+      ],
+      "is_zaned": 0,
+      "share_url": "http://yuxin.yddapp.com/pond/index/feed?id=146166-4a64020f1fbd008362bf39c937df2108",
+      "is_favorite": 0
+    }
+  }
+}
